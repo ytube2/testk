b744b155f7807042c8c8469a3ccad572e10bd520
jhall031 <85783577+jhall031@users.noreply.github.com>
2021-06-15 17:27:49 +0700

    Update sodosok.sh

--- sodosok.sh
+++ sodosok.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#shadowsocks-libev obfs install by Horasss
+#shadowsocks-libev obfs install by dKusya
 source /etc/os-release
 OS=$ID
 ver=$VERSION_ID
@@ -90,11 +90,11 @@ ip6tables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEP
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
-wget -O addss "https://raw.githubusercontent.com/kevindoni/AutoScriptSSH/main/addss.sh"
-wget -O delss "https://raw.githubusercontent.com/kevindoni/AutoScriptSSH/main/delss.sh"
-wget -O cekss "https://raw.githubusercontent.com/kevindoni/AutoScriptSSH/main/cekss.sh"
-wget -O xp-ss "https://raw.githubusercontent.com/kevindoni/AutoScriptSSH/main/xp-ss.sh"
-wget -O renewss "https://raw.githubusercontent.com/kevindoni/AutoScriptSSH/main/renewss.sh"
+wget -O addss "https://raw.githubusercontent.com/jhall031/testk/main/addss.sh"
+wget -O delss "https://raw.githubusercontent.com/jhall031/testk/main/delss.sh"
+wget -O cekss "https://raw.githubusercontent.com/jhall031/testk/main/cekss.sh"
+wget -O xp-ss "https://raw.githubusercontent.com/jhall031/testk/main/xp-ss.sh"
+wget -O renewss "https://raw.githubusercontent.com/jhall031/testk/main/renewss.sh"
 chmod +x addss
 chmod +x delss
 chmod +x cekss
