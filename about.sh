ef02b3d10e391e0796ac0e24ba738fc164b2e339
jhall031 <85783577+jhall031@users.noreply.github.com>
2021-06-15 16:33:58 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit               #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by dKusya                            #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Allah                                         #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Owner VPS                                     #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
