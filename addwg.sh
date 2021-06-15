c5d23d6bd06960f6681dcec622785f65a619c953
jhall031 <85783577+jhall031@users.noreply.github.com>
2021-06-15 16:41:00 +0700

    Update addwg.sh

--- addwg.sh
+++ addwg.sh
@@ -79,5 +79,5 @@ AllowedIPs = $CLIENT_ADDRESS/32" >>"/etc/wireguard/$SERVER_WG_NIC.conf"
 	echo -e "==============================="
 	echo -e "Expired On     : $exp"
 	echo -e ""
-	echo -e "By KEVIN DONI"
+	echo -e "By dKusya"
 	rm -f /root/wg0-client-$CLIENT_NAME.conf
