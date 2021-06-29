90f8960583f656d4ec0bddb4ffae6f7ab209eff2
jhall031 <85783577+jhall031@users.noreply.github.com>
2021-06-29 08:04:05 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -86,7 +86,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git
 echo "clear" >> .profile
 echo "neofetch" >> .profile
-echo "echo by KEVIN DONI" >> .profile
+echo "echo by dkusya" >> .profile
 
 # install webserver
 apt -y install nginx
@@ -95,7 +95,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/jhall031/testk/main/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by KEVIN DONI</pre>" > /home/vps/public_html/index.html
+echo "<pre>Setup by dkusya</pre>" > /home/vps/public_html/index.html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/jhall031/testk/main/vps.conf"
 /etc/init.d/nginx restart
 
